0359e98ad18037a9b83fe86b6a4e254d9b62a846
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-09-08 16:53:18 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import {
   Subheadline,
   Chip,
   IconButton,
+  Headline,
+  Title,
 } from "@telegram-apps/telegram-ui";
 
 // import { Icon32ProfileColoredSquare } from "@telegram-apps/telegram-ui/dist/icons/28/chat";
@@ -21,218 +23,217 @@ import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <List>
+    <div>
       <SearchBar />
-      <Section header="yvyt">
-        <div
+
+      <div
+        style={{
+          margin: 9,
+          background: " var(--tgui--bg_color)   ",
+          borderRadius: 20,
+          overflow: "hidden",
+        }}
+      >
+        <Cell
+          interactiveAnimation="background"
+          before={
+            <Avatar
+              size={48}
+              src="https://avatars.githubusercontent.com/u/84640980?v=4"
+            />
+          }
+          subtitle=<Subheadline level="2">
+            Fullstack Developer & Backend Developer
+          </Subheadline>
+          after={
+            <div style={{ width: 45 }}>
+              <IconButton size="l" mode="bezeled">
+                <Icon28Chat />
+              </IconButton>
+            </div>
+          }
+        >
+          <Headline weight="1">ross96D</Headline>
+        </Cell>
+        <ButtonCell
           style={{
-            margin: 9,
-            background: "var(--tgui--secondary_bg_color)",
-            borderRadius: 20,
-            overflow: "hidden",
+            display: "flex",
+            overflow: "scroll",
+            flexWrap: "nowrap",
+            paddingBlock: 2,
+            alignItems: "flex-start",
+            height: 60,
           }}
         >
-          <Cell
-            before={
-              <Avatar
-                size={48}
-                src="https://avatars.githubusercontent.com/u/84640980?v=4"
-              />
-            }
-            subtitle=<Subheadline level="2">
-              Fullstack Developer & Backend Developer
-            </Subheadline>
-            after={
-              <div style={{ width: 45 }}>
-                <IconButton size="l" mode="bezeled">
-                  <Icon28Chat />
-                </IconButton>
-              </div>
-            }
-          >
-            <Caption weight="1" level="1">
-              ross96D
-            </Caption>
-          </Cell>
-          <ButtonCell
-            style={{
-              display: "flex",
-              overflow: "scroll",
-              flexWrap: "nowrap",
-              paddingBlock: 2,
-              alignItems: "flex-start",
-              height: 60,
-            }}
-          >
-            <div style={{ display: "flex", gap: 8 }}>
-              <Chip mode="outline">JavaScript</Chip>
-              <Chip mode="outline">Go</Chip>
-              <Chip mode="outline">Dart</Chip>
-              <Chip mode="outline">Python</Chip>
-              <Chip mode="outline">Lua</Chip>
-              <Chip mode="outline">Rust</Chip>
-              <Chip mode="outline">Action</Chip>
-              <Chip mode="outline">Action</Chip>
+          <div style={{ display: "flex", gap: 8 }}>
+            <Chip mode="outline">JavaScript</Chip>
+            <Chip mode="outline">Go</Chip>
+            <Chip mode="outline">Dart</Chip>
+            <Chip mode="outline">Python</Chip>
+            <Chip mode="outline">Lua</Chip>
+            <Chip mode="outline">Rust</Chip>
+            <Chip mode="outline">Action</Chip>
+            <Chip mode="outline">Action</Chip>
+          </div>
+        </ButtonCell>
+      </div>
+      <div
+        style={{
+          margin: 9,
+          background: " var(--tgui--bg_color)   ",
+          borderRadius: 20,
+          overflow: "hidden",
+        }}
+      >
+        <Cell
+          interactiveAnimation="opacity"
+          before={
+            <Avatar
+              size={48}
+              src="https://avatars.githubusercontent.com/u/84640980?v=4"
+            />
+          }
+          subtitle=<Subheadline level="2">
+            Fullstack Developer & Backend Developer
+          </Subheadline>
+          after={
+            <div style={{ width: 45 }}>
+              <IconButton size="l" mode="bezeled">
+                <Icon28Chat />
+              </IconButton>
             </div>
-          </ButtonCell>
-        </div>
-        <div
+          }
+        >
+          <Caption weight="1" level="1">
+            ross96D
+          </Caption>
+        </Cell>
+        <ButtonCell
           style={{
-            margin: 9,
-            background: "var(--tgui--secondary_bg_color)",
-            borderRadius: 20,
-            overflow: "hidden",
+            display: "flex",
+            overflow: "scroll",
+            flexWrap: "nowrap",
+            paddingBlock: 2,
+            alignItems: "flex-start",
+            height: 60,
           }}
         >
-          <Cell
-            before={
-              <Avatar
-                size={48}
-                src="https://avatars.githubusercontent.com/u/84640980?v=4"
-              />
-            }
-            subtitle=<Subheadline level="2">
-              Fullstack Developer & Backend Developer
-            </Subheadline>
-            after={
-              <div style={{ width: 45 }}>
-                <IconButton size="l" mode="bezeled">
-                  <Icon28Chat />
-                </IconButton>
-              </div>
-            }
-          >
-            <Caption weight="1" level="1">
-              ross96D
-            </Caption>
-          </Cell>
-          <ButtonCell
-            style={{
-              display: "flex",
-              overflow: "scroll",
-              flexWrap: "nowrap",
-              paddingBlock: 2,
-              alignItems: "flex-start",
-              height: 60,
-            }}
-          >
-            <div style={{ display: "flex", gap: 8 }}>
-              <Chip mode="outline">JavaScript</Chip>
-              <Chip mode="outline">Go</Chip>
-              <Chip mode="outline">Dart</Chip>
-              <Chip mode="outline">Python</Chip>
-              <Chip mode="outline">Lua</Chip>
-              <Chip mode="outline">Rust</Chip>
-              <Chip mode="outline">Action</Chip>
-              <Chip mode="outline">Action</Chip>
+          <div style={{ display: "flex", gap: 8 }}>
+            <Chip mode="outline">JavaScript</Chip>
+            <Chip mode="outline">Go</Chip>
+            <Chip mode="outline">Dart</Chip>
+            <Chip mode="outline">Python</Chip>
+            <Chip mode="outline">Lua</Chip>
+            <Chip mode="outline">Rust</Chip>
+            <Chip mode="outline">Action</Chip>
+            <Chip mode="outline">Action</Chip>
+          </div>
+        </ButtonCell>
+      </div>
+      <div
+        style={{
+          margin: 9,
+          background: " var(--tgui--bg_color)   ",
+          borderRadius: 20,
+          overflow: "hidden",
+        }}
+      >
+        <Cell
+          before={
+            <Avatar
+              size={48}
+              src="https://avatars.githubusercontent.com/u/84640980?v=4"
+            />
+          }
+          subtitle=<Subheadline level="2">
+            Fullstack Developer & Backend Developer
+          </Subheadline>
+          after={
+            <div style={{ width: 45 }}>
+              <IconButton size="l" mode="bezeled">
+                <Icon28Chat />
+              </IconButton>
             </div>
-          </ButtonCell>
-        </div>
-        <div
+          }
+        >
+          <Caption weight="1" level="1">
+            ross96D
+          </Caption>
+        </Cell>
+        <ButtonCell
           style={{
-            margin: 9,
-            background: "var(--tgui--secondary_bg_color)",
-            borderRadius: 20,
-            overflow: "hidden",
+            display: "flex",
+            overflow: "scroll",
+            flexWrap: "nowrap",
+            paddingBlock: 2,
+            alignItems: "flex-start",
+            height: 60,
           }}
         >
-          <Cell
-            before={
-              <Avatar
-                size={48}
-                src="https://avatars.githubusercontent.com/u/84640980?v=4"
-              />
-            }
-            subtitle=<Subheadline level="2">
-              Fullstack Developer & Backend Developer
-            </Subheadline>
-            after={
-              <div style={{ width: 45 }}>
-                <IconButton size="l" mode="bezeled">
-                  <Icon28Chat />
-                </IconButton>
-              </div>
-            }
-          >
-            <Caption weight="1" level="1">
-              ross96D
-            </Caption>
-          </Cell>
-          <ButtonCell
-            style={{
-              display: "flex",
-              overflow: "scroll",
-              flexWrap: "nowrap",
-              paddingBlock: 2,
-              alignItems: "flex-start",
-              height: 60,
-            }}
-          >
-            <div style={{ display: "flex", gap: 8 }}>
-              <Chip mode="outline">JavaScript</Chip>
-              <Chip mode="outline">Go</Chip>
-              <Chip mode="outline">Dart</Chip>
-              <Chip mode="outline">Python</Chip>
-              <Chip mode="outline">Lua</Chip>
-              <Chip mode="outline">Rust</Chip>
-              <Chip mode="outline">Action</Chip>
-              <Chip mode="outline">Action</Chip>
+          <div style={{ display: "flex", gap: 8 }}>
+            <Chip mode="outline">JavaScript</Chip>
+            <Chip mode="outline">Go</Chip>
+            <Chip mode="outline">Dart</Chip>
+            <Chip mode="outline">Python</Chip>
+            <Chip mode="outline">Lua</Chip>
+            <Chip mode="outline">Rust</Chip>
+            <Chip mode="outline">Action</Chip>
+            <Chip mode="outline">Action</Chip>
+          </div>
+        </ButtonCell>
+      </div>
+      <div
+        style={{
+          margin: 9,
+          background: " var(--tgui--bg_color)   ",
+          borderRadius: 20,
+          overflow: "hidden",
+        }}
+      >
+        <Cell
+          before={
+            <Avatar
+              size={48}
+              src="https://avatars.githubusercontent.com/u/84640980?v=4"
+            />
+          }
+          subtitle=<Subheadline level="2">
+            Fullstack Developer & Backend Developer
+          </Subheadline>
+          after={
+            <div style={{ width: 45 }}>
+              <IconButton size="l" mode="bezeled">
+                <Icon28Chat />
+              </IconButton>
             </div>
-          </ButtonCell>
-        </div>
-        <div
+          }
+        >
+          <Caption weight="1" level="1">
+            ross96D
+          </Caption>
+        </Cell>
+        <ButtonCell
           style={{
-            margin: 9,
-            background: "var(--tgui--secondary_bg_color)",
-            borderRadius: 20,
-            overflow: "hidden",
+            display: "flex",
+            overflow: "scroll",
+            flexWrap: "nowrap",
+            paddingBlock: 2,
+            alignItems: "flex-start",
+            height: 60,
           }}
         >
-          <Cell
-            before={
-              <Avatar
-                size={48}
-                src="https://avatars.githubusercontent.com/u/84640980?v=4"
-              />
-            }
-            subtitle=<Subheadline level="2">
-              Fullstack Developer & Backend Developer
-            </Subheadline>
-            after={
-              <div style={{ width: 45 }}>
-                <IconButton size="l" mode="bezeled">
-                  <Icon28Chat />
-                </IconButton>
-              </div>
-            }
-          >
-            <Caption weight="1" level="1">
-              ross96D
-            </Caption>
-          </Cell>
-          <ButtonCell
-            style={{
-              display: "flex",
-              overflow: "scroll",
-              flexWrap: "nowrap",
-              paddingBlock: 2,
-              alignItems: "flex-start",
-              height: 60,
-            }}
-          >
-            <div style={{ display: "flex", gap: 8 }}>
-              <Chip mode="outline">JavaScript</Chip>
-              <Chip mode="outline">Go</Chip>
-              <Chip mode="outline">Dart</Chip>
-              <Chip mode="outline">Python</Chip>
-              <Chip mode="outline">Lua</Chip>
-              <Chip mode="outline">Rust</Chip>
-              <Chip mode="outline">Action</Chip>
-              <Chip mode="outline">Action</Chip>
-            </div>
-          </ButtonCell>
-        </div>
-      </Section>
-    </List>
+          <div style={{ display: "flex", gap: 8 }}>
+            <Chip mode="outline">JavaScript</Chip>
+            <Chip mode="outline">Go</Chip>
+            <Chip mode="outline">Dart</Chip>
+            <Chip mode="outline">Python</Chip>
+            <Chip mode="outline">Lua</Chip>
+            <Chip mode="outline">Rust</Chip>
+            <Chip mode="outline">Action</Chip>
+            <Chip mode="outline">Action</Chip>
+          </div>
+        </ButtonCell>
+      </div>
+    </div>
   );
 }
